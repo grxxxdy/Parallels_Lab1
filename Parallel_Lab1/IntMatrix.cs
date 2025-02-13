c07affd55417259a722a2a5a045660aa0ee5a815
grxxxdy <mozjukhin@gmail.com>
2025-02-13 18:27:57 +0200

    non-parallel approach

---
+++ Parallel_Lab1/IntMatrix.cs
@@ -0,0 +1,59 @@
+﻿namespace Parallel_Lab1;
+
+public class IntMatrix
+{
+    private List<List<int>> matrix;
+    private int _rows;
+    private int _columns;
+
+    public IntMatrix(int rows, int columns)
+    {
+        _rows = rows;
+        _columns = columns;
+        
+        matrix = new List<List<int>>(rows);
+        for (int i = 0; i < rows; i++)
+        {
+            matrix.Add(new List<int>(new int[columns]));
+        }
+    }
+
+    public void FillMatrixWithRands()
+    {
+        Random rand = new Random();
+        
+        for (int i = 0; i < _rows; i++)
+        {
+            for (int j = 0; j < _columns; j++)
+            {
+                matrix[i][j] = rand.Next(1, 1001);
+            }
+        }
+    }
+
+    public void PrintMatrix()
+    {
+        for (int i = 0; i < _rows; i++)
+        {
+            for (int j = 0; j < _columns; j++)
+            {
+                Console.Write(matrix[i][j] + "\t");
+            }
+            
+            Console.WriteLine();
+        }
+        
+        Console.WriteLine();
+    }
+
+    public void CalculateC(IntMatrix A, IntMatrix B, int k)
+    {
+        for (int i = 0; i < _rows; i++)
+        {
+            for (int j = 0; j < _columns; j++)
+            {
+                matrix[i][j] = A.matrix[i][j] - k * B.matrix[i][j];
+            }
+        }
+    }
+}
--- Parallel_Lab1/Program.cs
+++ Parallel_Lab1/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace Parallel_Lab1;
+﻿using System.Diagnostics;
+
+namespace Parallel_Lab1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int rows = 20000, columns = 30000, k = 13;
+
+        IntMatrix A = new IntMatrix(rows, columns);
+        IntMatrix B = new IntMatrix(rows, columns);
+        IntMatrix C = new IntMatrix(rows, columns);
+        
+        A.FillMatrixWithRands();
+        B.FillMatrixWithRands();
+        
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        C.CalculateC(A, B, k);
+
+        stopwatch.Stop();
+        Console.WriteLine($"Rows: {rows}\tColumns: {columns}\nTime elapsed: {stopwatch.ElapsedMilliseconds} ms\n");
+
+        // Console.WriteLine("Matrix A:");
+        // A.PrintMatrix();
+        //
+        // Console.WriteLine("Matrix B:");
+        // B.PrintMatrix();
+        //
+        // Console.WriteLine("Matrix C:");
+        // C.PrintMatrix();
     }
 }
